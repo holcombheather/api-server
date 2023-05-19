b710bb6186a20c15fc40fccd408ec5bd6fbacb14
Heather Holcomb <holcombheather@gmail.com>
2023-05-18 22:35:45 -0700

    updated customer.js

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,17 +1,19 @@
 'use strict';
 
 module.exports = (sequelizeDatabase, DataTypes) => {
-    // note that customers will be the name of the table created, pluralized
-    return sequelizeDatabase.define('customers', {
-        name: {
-            type: DataTypes:STRING,
-            allowNull: false,
-        },
-        age: {
-
-        },
-        pronouns: {
-            
-        }
-    })
-}
+  // note that customers will be the name of the table created, pluralized
+  return sequelizeDatabase.define('customers', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    pronouns: {
+      type: DataTypes.ENUM,
+      values: ['they/them', 'he/them', 'she/her', 'he/him'],
+    },
+  });
+};
